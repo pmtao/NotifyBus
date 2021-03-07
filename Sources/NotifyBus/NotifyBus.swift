ac8dba336fcdf34d57405a2d1c7fc25667289be1
Meler Paine <pmtnmd@gmail.com>
2021-03-07 21:22:45 +0800

    新增延迟通知的处理模式，累积多条延迟通知时，可指定只执行哪几条通知。

--- Sources/NotifyBus/NotifyBus.swift
+++ Sources/NotifyBus/NotifyBus.swift
@@ -7,15 +7,19 @@
 
 import Foundation
 
+public enum HandleMode {
+  case all
+  case onlyFirst
+  case onlyLast
+  case first(Int)
+  case last(Int)
+}
+
 class NotifyHandler {
   var handlers: [() -> Void]
   
   init(_ handler: @escaping () -> Void) {
     self.handlers = [handler]
-  }
-  
-  func addHanlder(_ handler: @escaping () -> Void) {
-    handlers.append(handler)
   }
   
   func execute() {
@@ -44,13 +48,14 @@ public class NotifyBus {
   }
   
   public func observe<T: BasicNotifyObject>(_ notifyObject: T,
-                                     queue: OperationQueue? = nil,
-                                     runAtOnce: Bool = true,
-                                     handler: @escaping (T) -> Void) {
+                                            queue: OperationQueue? = nil,
+                                            executeLater: Bool = false,
+                                            handleMode: HandleMode = .all,
+                                            handler: @escaping (T) -> Void) {
     let token = notifyObject.notifyCenter.addObserver(forName: notifyObject.name,
                                                       object: nil,
                                                       queue: queue) {_ in
-      if runAtOnce {
+      if !executeLater {
         handler(notifyObject)
       } else {
         let handlerQueue = (queue != nil ? queue : OperationQueue.current) ?? OperationQueue()
@@ -60,21 +65,22 @@ public class NotifyBus {
           }
         }
         
-        self.addDelayedHandler(of: notifyObject.name, delayedHandler: delayedHandler)
+        self.addDelayedHandler(of: notifyObject.name, handleMode: handleMode, delayedHandler: delayedHandler)
       }
     }
     self.addToken(in: notifyObject.notifyCenter, token: token)
   }
   
   public func observe<T: InfomedNotifyObject>(_ notifyObject: T,
-                                       queue: OperationQueue? = nil,
-                                       runAtOnce: Bool = true,
-                                       handler: @escaping (T) -> Void) {
+                                              queue: OperationQueue? = nil,
+                                              executeLater: Bool = false,
+                                              handleMode: HandleMode = .all,
+                                              handler: @escaping (T) -> Void) {
     let token = notifyObject.notifyCenter.addObserver(forName: notifyObject.name,
                                                       object: nil,
                                                       queue: queue) {
       (_notification:Notification) in
-      if runAtOnce {
+      if !executeLater {
         handler(notifyObject.getObject(notification: _notification))
       } else {
         let handlerQueue = (queue != nil ? queue : OperationQueue.current) ?? OperationQueue()
@@ -84,18 +90,64 @@ public class NotifyBus {
           }
         }
 
-        self.addDelayedHandler(of: notifyObject.name, delayedHandler: delayedHandler)
+        self.addDelayedHandler(of: notifyObject.name, handleMode: handleMode, delayedHandler: delayedHandler)
       }
     }
     self.addToken(in: notifyObject.notifyCenter, token: token)
   }
   
-  private func addDelayedHandler(of name: Notification.Name, delayedHandler: @escaping () -> Void) {
+  private func addDelayedHandler(of name: Notification.Name,
+                                 handleMode: HandleMode = .all,
+                                 delayedHandler: @escaping () -> Void) {
     if self.delayedHandlers[name] != nil {
-      self.delayedHandlers[name]!.addHanlder(delayedHandler)
+      let currentHandlerCount = self.delayedHandlers[name]!.handlers.count
+      switch handleMode {
+      case .all:
+        self.delayedHandlers[name]!.handlers.append(delayedHandler)
+      case .first(let count):
+        guard count > 0 else { return }
+        if currentHandlerCount >= count {
+          self.delayedHandlers[name]!.handlers.removeSubrange(count...)
+        } else {
+          self.delayedHandlers[name]!.handlers.append(delayedHandler)
+        }
+      case .onlyFirst:
+        if currentHandlerCount >= 1 {
+          self.delayedHandlers[name]!.handlers.removeSubrange(1...)
+        } else {
+          self.delayedHandlers[name]!.handlers.append(delayedHandler)
+        }
+      case .last(let count):
+        guard count > 0 else { return }
+        if currentHandlerCount >= count {
+          self.delayedHandlers[name]!.handlers.append(delayedHandler)
+          self.delayedHandlers[name]!.handlers.removeFirst(currentHandlerCount - count + 1)
+        } else {
+          self.delayedHandlers[name]!.handlers.append(delayedHandler)
+        }
+      case .onlyLast:
+        if currentHandlerCount >= 1 {
+          self.delayedHandlers[name]!.handlers = [delayedHandler]
+        } else {
+          self.delayedHandlers[name]!.handlers.append(delayedHandler)
+        }
+      }
     } else {
-      let notifyHandler = NotifyHandler(delayedHandler)
-      self.delayedHandlers[name] = notifyHandler
+      switch handleMode {
+      case .all, .onlyFirst, .onlyLast:
+        let notifyHandler = NotifyHandler(delayedHandler)
+        self.delayedHandlers[name] = notifyHandler
+      case .first(let count):
+        if count > 0 {
+          let notifyHandler = NotifyHandler(delayedHandler)
+          self.delayedHandlers[name] = notifyHandler
+        }
+      case .last(let count):
+        if count > 0 {
+          let notifyHandler = NotifyHandler(delayedHandler)
+          self.delayedHandlers[name] = notifyHandler
+        }
+      }
     }
   }
   
--- Tests/NotifyBusTests/NotifyBusTests.swift
+++ Tests/NotifyBusTests/NotifyBusTests.swift
@@ -22,32 +22,124 @@ enum UserNotification: BasicNotifyObject {
   
 }
 
+enum MessageNotification: InfomedNotifyObject {
+  case commonMessage(String?)
+  case secreteMessage(String?)
+  
+  var notifyCenter: NotificationCenter {
+    return userNotificationCenter
+  }
+  
+  var name: Notification.Name {
+    switch self {
+    case .commonMessage:
+      return Notification.Name("CommonMessageNotificationName")
+    case .secreteMessage:
+      return Notification.Name("SecreteMessageNotificationName")
+    }
+  }
+  
+  var userInfoKey: String {
+    switch self {
+    case .commonMessage, .secreteMessage:
+      return "message"
+    }
+  }
+  
+  var userInfo: [String : Any]? {
+    switch self {
+    case .commonMessage(let msg):
+      return msg == nil ? nil : [userInfoKey: msg!]
+    case .secreteMessage(let msg):
+      return msg == nil ? nil : [userInfoKey: msg!]
+    }
+  }
+  
+  func getObject(notification: Notification) -> MessageNotification {
+    switch self {
+    case .commonMessage:
+      let userInfo = notification.userInfo!
+      let msg = userInfo[self.userInfoKey]! as! String
+      return .commonMessage(msg)
+    case .secreteMessage:
+      let userInfo = notification.userInfo!
+      let msg = userInfo[self.userInfoKey]! as! String
+      return .secreteMessage(msg)
+    }
+  }
+}
+
 
 final class NotifyBusTests: XCTestCase {
-  var testNum = 0
-  var expectation: XCTestExpectation?
   
-  func testExample() {
-    expectation = self.expectation(description: "UserNotification test")
+  
+  func testExecuteLater() {
+    var testNum = 0
+    let expectation = self.expectation(description: "UserNotification test")
     let notifyBus = NotifyBus()
-    notifyBus.observe(UserNotification.infoChanged, runAtOnce: false, handler: userInfoChangedNotify)
+    notifyBus.observe(UserNotification.infoChanged, executeLater: true) { _ in
+      testNum += 1
+      XCTAssert(testNum == 1)
+      expectation.fulfill()
+      print("===testExecuteLater complete.===")
+    }
     
     UserNotification.infoChanged.post()
     DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
       notifyBus.execute(UserNotification.infoChanged.name)
     }
     
-    self.wait(for: [expectation!], timeout: 2)
+    self.wait(for: [expectation], timeout: 2)
   }
   
-  func userInfoChangedNotify(notificationInfo: UserNotification) {
-    testNum += 1
-    XCTAssert(self.testNum == 1)
-    expectation!.fulfill()
-    print("===userInfoChangedNotify complete.===")
+  func testExecuteLaterFirst() {
+    var testNum = 2
+    let expectation = self.expectation(description: "UserNotification test")
+    let notifyBus = NotifyBus()
+    notifyBus.observe(UserNotification.infoChanged, executeLater: true, handleMode: .first(1)) { _ in
+      testNum *= 2
+    }
+    UserNotification.infoChanged.post()
+    UserNotification.infoChanged.post()
+    
+    DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+      notifyBus.execute(UserNotification.infoChanged.name)
+    }
+    DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+      XCTAssert(testNum == 4)
+      expectation.fulfill()
+      print("===testExecuteLaterFirst complete.===")
+    }
+    self.wait(for: [expectation], timeout: 3)
+  }
+  
+  func testExecuteLaterLast() {
+    var msg = [String]()
+    let expectation = self.expectation(description: "MessageNotification test")
+    let notifyBus = NotifyBus()
+    notifyBus.observe(MessageNotification.commonMessage(nil), executeLater: true, handleMode: .onlyLast) {
+      notification in
+      guard case let .commonMessage(_msg) = notification, let newMsg = _msg else { return }
+      msg.append(newMsg)
+    }
+    MessageNotification.commonMessage("good").post()
+    MessageNotification.commonMessage("bad").post()
+    MessageNotification.commonMessage("nice").post()
+    
+    DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+      notifyBus.execute(MessageNotification.commonMessage(nil).name)
+    }
+    DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+      XCTAssert(msg == ["nice"])
+      expectation.fulfill()
+      print("===testExecuteLaterLast complete.===")
+    }
+    self.wait(for: [expectation], timeout: 3)
   }
   
   static var allTests = [
-    ("testExample", testExample),
+    ("testExecuteLater", testExecuteLater),
+    ("testExecuteLaterFirst", testExecuteLaterFirst),
+    ("testExecuteLaterLast", testExecuteLaterLast),
   ]
 }
